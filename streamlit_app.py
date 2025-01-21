707291be61f6b79a5193c59decd119d72ece90ed
JimmyeJones <123339136+JimmyeJones@users.noreply.github.com>
2025-01-21 00:35:52 +0000

    Pending changes exported from your codespace

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,25 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Welcome to A Geography Channel Games")
+page = st.sidebar.selectbox("Page", ["Home", "Buy?", "Maps", "About"])
+if page == "Maps":
+    game = st.sidebar.selectbox("Select Game", ["Europe 2025", "The Americas 2025", "Africa 2025", "Asia 2025", "Southeast Asia + Oceania 2025"])
+    if game == "Europe 2025":
+        st.image("Main Maps/europe map.png")
+    if game == "The Americas 2025":
+        st.image("Main Maps/The Americas map.png")
+    if game == "Africa 2025":
+        st.image("Main Maps/Africa map.png")
+    if game == "Asia 2025":
+        st.image("Main Maps/Asia map.png")
+    if game == "Southeast Asia + Oceania 2025":
+        st.image("Main Maps/seao map.png")
+#Homepage
+if page == "Home":
+    st.subheader("Home")
+#Map 1
+if page == "About":
+    st.subheader("Map 1")
+#Map 2
+if page == "Map 2":
+    st.subheader("Map 2")
